13dfb570d159410d2eb04867d3f443a1f0f9e1d9
marcus-38 <marcus@r38.se>
2024-10-20 09:41:52 +0200

    using platform from sys instead of argparse

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 import subprocess
-import argparse
+from sys import platform
 
 def RunConan(buildType):
 	subprocess.run((
@@ -13,24 +13,16 @@ def RunPremake(action, osType):
 	))
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	group = parser.add_mutually_exclusive_group()
-	group.add_argument("--windows", help="Create Visual Studio solution files", action="store_true")
-	group.add_argument("--mac", help="Create Xcode project files", action="store_true")
-	group.add_argument("--linux", help="Create Makefiles", action="store_true")
-	
-	args = parser.parse_args()
-
 	RunConan("Debug")
 	RunConan("Release")
 
-	if args.windows:
+	if platform == "win32":
 		print("Creating Visual Studio solution files")
 		RunPremake("vs2022", "Windows")
-	elif args.mac:
+	elif platform == "darwin":
 		print("Creating Xcode project files")
 		RunPremake("xcode4", "macOS")
-	elif args.linux:
+	elif platform == "linux" or platform == "linux2":
 		print("Creating Makefile files")
 		RunPremake("gmake2", "Linux")
 	
